2a2989113f32195853f898cc490d1d351b6e37da
chibuzor <incovenant@hotmail.com>
2025-05-11 12:13:08 +0100

    second commit

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -1,14 +1,3 @@
-// export default function ProjectCard({ title, description, imageUrl }) {
-// 	return (
-// 		<div className="border rounded-lg overflow-hidden  shadow-lg">
-// 			<img src={imageUrl} alt={title} className="w-full h-48 mt-4" />
-// 			<div className="p-5">
-// 				<h2 className="text-xl font-semibold mb-3">{title}</h2>
-// 				<p className="text-gray-500">{description}</p>
-// 			</div>
-// 		</div>
-// 	);
-// }
 import Image from "next/image";
 
 export default function ProjectCard({ project }) {
